c0e1dadf11458f31a5763f8993dceb8aac9239c7
ClaraShar <179800587@qq.com>
2019-02-19 14:08:53 +0800

    2019-2-19(DP)

---
+++ 练习系统/ALGO-122.cpp
@@ -0,0 +1,21 @@
+//f(m-1,n)+f(m,n-1),还鞋的在第一个+租鞋的在第一个
+
+#include <iostream>
+using namespace std;
+
+int DFS(int m,int n)
+{
+	if(m<n)
+		return 0;
+	if(n==0)
+		return 1;
+	return DFS(m-1,n)+DFS(m,n-1);
+}
+
+int main()
+{
+	int m,n;
+	cin>>m>>n;
+	cout<<DFS(m,n);
+	return 0;
+}
