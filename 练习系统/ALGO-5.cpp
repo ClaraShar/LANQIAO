302066d5ada4f5815c3276a9cd4776679515d275
ClaraShar <179800587@qq.com>
2019-02-25 15:08:39 +0800

    2019-2-25

--- 练习系统/ALGO-5.cpp
+++ 练习系统/ALGO-5.cpp
@@ -1,28 +1,30 @@
+//Bellman-Ford算法（运行超时）
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main()
 {
-	int n,m,inf=99999999,dis[20001]={0},u[200001],v[200001],w[200001];//dis[i]代表第一点到第i点距离，u[i]，v[i],w[i]分别代表第i条边的起始点，终点和长度。
+	int u[200001],v[200001],w[200001],dis[20001];
+	memset(dis,10001,sizeof(dis));
+	dis[1]=0;
+	int n,m;
 	cin>>n>>m;
-	fill(dis+2,dis+20001,inf);
-	for(int i=1;i<=m;i++)
+	for(int i=1;i<=m;++i)
 		cin>>u[i]>>v[i]>>w[i];
-	for(int i=1;i<=n-1;i++)
+	for(int i=1;i<n;i++)
 	{
-        int check=0;
-        for(int j=1;j<=m;j++)
-        {
-            if(dis[v[j]]>dis[u[i]]+w[i])
-            {
-                dis[v[j]]=dis[u[j]]+w[j];
+		int check=0;//添加check变量量判断本轮是否进⾏松弛了，如果未进行松弛则可以提前退出循环(否则会超时)
+		for(int j=1;j<=m;++j)
+			if(dis[v[j]]>dis[u[j]]+w[j])
+			{
+				dis[v[j]]=dis[u[j]]+w[j];
 				check=1;
 			}
-		}
-        if(check==0)
-        	break;
-    }
-    for(int i=2;i<=n;i++)
-        cout<<dis[i];
+		if(check==0)
+			break;
+	}
+	for(int i=2;i<=n;i++)
+		cout<<dis[i]<<endl;
 	return 0;
 }
