9e5682913e922aef6074db0212e8afc522d990b8
ClaraShar <179800587@qq.com>
2019-02-26 16:56:57 +0800

    2019-2-26

---
+++ 练习系统/ALGO-48.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	int a[n+1][m+1];
+	memset(a,0,sizeof(a));
+	for(int i=0;i<m;++i)
+	{
+		int j,k;
+		cin>>j>>k;
+		a[j-1][i]=1;
+		a[k-1][i]=-1;
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;++j)
+			cout<<a[i][j]<<" ";
+		cout<<endl;
+	}
+	cin>>n;
+	return 0;
+}
